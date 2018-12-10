78fb3fd5aa56ac3b77e38694e09883a843e816eb
Mathieu Debove <mathieu.debove@sqits.net>
2018-12-10 15:24:09 +0000

    Initial implementation of Lambda function to bootstrap a Flogo Web UI

---
+++ src/aws-lambda-bootstrap-flogo/imports.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+
+	_ "github.com/TIBCOSoftware/flogo-contrib/trigger/lambda"
+	_ "github.com/TIBCOSoftware/flogo-contrib/action/flow"
+	_ "github.com/TIBCOSoftware/flogo-contrib/activity/log"
+	_ "github.com/square-it/flogo-contrib-activities/command"
+	_ "github.com/TIBCOSoftware/flogo-contrib/activity/log"
+	_ "github.com/TIBCOSoftware/flogo-contrib/activity/actreturn"
+
+)
